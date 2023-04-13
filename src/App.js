06eeb63fc6d733d6b9794aa1fdc0f4dce11ca618
akansha-1202 <akanshav325@gmail.com>
2023-04-13 13:31:20 +0530

    hands pn 7t

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Routing from './Routes/Routing'
+import Navbar from './Component/Navbar'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <Navbar/>
+      <Routing/> 
+    </>
+  )
+}
+
+export default App
